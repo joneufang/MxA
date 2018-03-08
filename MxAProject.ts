c955f6accb919d182571a7bc2e8dc56313fb1c1f
joneufang <37178944+joneufang@users.noreply.github.com>
2018-03-08 13:11:12 +0100

    Init TsFiles

---
+++ MxAProject.ts
@@ -0,0 +1,5 @@
+import {ModelSdkClient, IModel, IModelUnit, domainmodels, utils, pages, customwidgets, projects} from "mendixmodelsdk";
+import {MendixSdkClient, Project, OnlineWorkingCopy, loadAsPromise} from "mendixplatformsdk";
+import when = require("when");
+
+
---
+++ test.ts
@@ -0,0 +1,5 @@
+const username = 'jochen.neufang@mansystems.de';
+const apikey = 'e6a890bf-6377-4395-8924-87bfe8da7330';
+// Please change your project Id and name to something you prefer.
+let projectName = `7-2-0-ATSTest`;
+let projectId = `01bfc705-81e4-4ffa-8bc9-0c43e7f2b5ba`;
