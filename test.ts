deea49a926485449f94bbdd7c0e0c248b1253b38
joneufang <37178944+joneufang@users.noreply.github.com>
2018-03-09 11:04:21 +0100

    Solved Conflict

--- test.ts
+++ test.ts
@@ -8,10 +8,7 @@ let projectName = `7-2-0-ATSTest`;
 let projectId = `01bfc705-81e4-4ffa-8bc9-0c43e7f2b5ba`;
 
 <<<<<<< HEAD
+
 let project = new mendixanalytics.MxAToTextFile(username, apikey, projectId, "./Test.txt");
-project.getDocumentsFromProject([qrycons.documents.propertys.ID], [qrycons.documents.filter.ID], ["Test"], [1]); project.getDocumentsFromProject([qrycons.documents.propertys.ID], [qrycons.documents.filter.ID], ["Test"], [1]); 
 project.getDocumentsFromProject([qrycons.documents.propertys.ID], [qrycons.documents.filter.ID], ["Test"], [1]); 
-=======
-let mxap = new mendixanalytics.MxAToTextFile(username, apikey, projectId, "./Test.txt");
-mxap.getDocumentsFromProject([mendixanalytics.constants.propertys.ID], [mendixanalytics.constants.filter.ID], ["Test"], [1]); 
->>>>>>> bf515b70d4c87acaf08e9496d9413713e7ee7710
+
